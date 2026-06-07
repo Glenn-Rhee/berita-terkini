4a86523d2a463c13401d5ec927d625a3724d2ae4
Glenn-Rhee <glennviktor5@gmail.com>
2026-06-07 18:13:31 +0700

    feat: added entertaint page

---
+++ src/app/entertaint/page.tsx
@@ -0,0 +1,22 @@
+import Card from "@/components/Card";
+import Container from "@/components/Container";
+import FooterSection from "@/components/FooterSection";
+import HeaderSection from "@/components/HeaderSection";
+
+export default function EntertaintPage() {
+  return (
+    <Container>
+      <section className="flex flex-col gap-y-8">
+        <HeaderSection title="Terbaru" />
+        <div className="flex flex-col gap-y-32 justify-between">
+          <div className="grid grid-cols-2 md:grid-cols-4 gap-16">
+            {Array.from({ length: 16 }).map((_, i) => (
+              <Card key={i} />
+            ))}
+          </div>
+          <FooterSection />
+        </div>
+      </section>
+    </Container>
+  );
+}
